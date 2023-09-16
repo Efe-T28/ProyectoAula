a15a46a4519fcbc2618e70b7d01fd4d451c4db3c
Efe-T28 <143205516+Efe-T28@users.noreply.github.com>
2023-09-15 19:30:41 -0500

    Proyecto

---
+++ ProyectoDeAula/src/proyectodeaula/Conductor.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectodeaula;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Conductor {
+    private String Nombre;
+    private double ID;
+
+    public Conductor(String Nombre, double ID) {
+        this.Nombre = Nombre;
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public double getID() {
+        return ID;
+    }
+
+    public void setID(double ID) {
+        this.ID = ID;
+    }
+}
---
+++ ProyectoDeAula/src/proyectodeaula/Costo.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectodeaula;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Costo {
+    private double PrecioRutaA;
+    private double PrecioRutaB;
+    private double PrecioRutaC;
+    private double Encargo;
+    private double Cargas;
+
+    public double getPrecioRutaA() {
+        return PrecioRutaA;
+    }
+
+    public void setPrecioRutaA(double PrecioRutaA) {
+        this.PrecioRutaA = PrecioRutaA;
+    }
+
+    public double getPrecioRutaB() {
+        return PrecioRutaB;
+    }
+
+    public void setPrecioRutaB(double PrecioRutaB) {
+        this.PrecioRutaB = PrecioRutaB;
+    }
+
+    public double getPrecioRutaC() {
+        return PrecioRutaC;
+    }
+
+    public void setPrecioRutaC(double PrecioRutaC) {
+        this.PrecioRutaC = PrecioRutaC;
+    }
+
+    public double getEncargo() {
+        return Encargo;
+    }
+
+    public void setEncargo(double Encargo) {
+        this.Encargo = Encargo;
+    }
+
+    public double getCargas() {
+        return Cargas;
+    }
+
+    public void setCargas(double Cargas) {
+        this.Cargas = Cargas;
+    }
+}
